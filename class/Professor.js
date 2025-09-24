baed651fb88fb6c0eb3f9f2b2dcf0d58d8980f70
Ana Heloisa <heloisaangelo.17@gmail.com>
2025-09-23 22:40:31 -0300

    Update Professor.js

---
+++ class/Professor.js
@@ -0,0 +1,8 @@
+const Usuario = require("./Usuario");
+
+class Professor extends Usuario {
+    acessoPainel(){
+        return "Painel do Professor: gerenciar notas e turmas"
+    }
+}
+module.exports = Professor
